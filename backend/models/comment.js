4044aa6d20a7906e507a805b5c99a4883a566fd5
Fouad DANI <dani2sm@gmail.com>
2017-08-24 22:19:04 +0100

    comment model

---
+++ backend/models/comment.js
@@ -0,0 +1,32 @@
+const sequelize = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const Comment = sequelize.define("comments", {
+    comment: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    isDeleted: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    },
+    deletedAt: DataTypes.DATE
+  });
+
+  Comment.associate = (models) => {
+    Comment.belongsTo(models.posts, {
+      onDelete: 'CASCADE',
+      foreignKey: {
+        field: "post_id",
+        allowNull: false
+      }
+    });
+  };
+
+
+  return Comment;
+};
+
